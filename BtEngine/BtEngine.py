e1c008079ca082b0bd01367481ec12887f67c1ae
GongJr0 <guneykiymac@gmail.com>
2025-01-08 02:50:29 +0100

    State-aware index based iteration
    WIP with known issues

--- BtEngine/BtEngine.py
+++ BtEngine/BtEngine.py
@@ -5,8 +5,7 @@ import numpy as np
 
 from datetime import datetime as dt
 
-from collections import Generator
-from typing import Optional
+from typing import Optional, Literal, Generator
 
 class BtEngine:
     def __init__(self, portfolio_close: pd.DataFrame, strategy: BtStrategy):
@@ -17,8 +16,8 @@ class BtEngine:
 
         self.price_data = portfolio_close
 
-        self.sma_period = 10
-        self.lma_period = 50
+        self.sma_period = 5
+        self.lma_period = 20
 
         self._iter = self._iterate()
         self.strat = strategy
@@ -29,11 +28,12 @@ class BtEngine:
             'lma': [self._ma, [self.lma_period]]
         }
 
+        self.holdings = {stock: 0 for stock in self.price_data.columns}  # init with 0 holdings
 
     def _get_args(self):
-        out = {}
+        out = []
         for arg in self.arg_signature:
-            out[arg] = self.arg_map[arg][0](*self.arg_map[arg][1])
+            out.append(self.arg_map[arg][0](*self.arg_map[arg][1]))
         return out
 
     def _ma(self, window):
@@ -44,8 +44,49 @@ class BtEngine:
         for i in range(len(self.price_data)):
             yield self.price_data.iloc[i]
 
-    def _trade(self):
-        ...
+    def _trade(self,
+               stock_name: str,
+               stock_price: float,
+               signal: Literal[-1, 0, 1]) -> None:
+        if signal == 1:
+            traded_cash = min(self.trade_proportion * self.cash, self.min_trade_price)  # Check for sufficient cash before buy
+            self.cash -= traded_cash
+            self.holdings[stock_name] += traded_cash / stock_price
 
-    def run(self):
-        ...
+        elif signal == -1:
+            signal_magnitude = self.trade_proportion * self.holdings[stock_name]  # In shares (not USD)
+            signal_magnitude = signal_magnitude if signal_magnitude > (self.min_trade_price/stock_price) else self.min_trade_price/stock_price
+
+            self.cash += signal_magnitude * stock_price
+            self.holdings[stock_name] -= signal_magnitude
+
+        elif signal == 0:
+            pass
+
+        else:
+            raise ValueError("Invalid signal value")
+
+    def run(self, strat: Literal['crossover'] = 'crossover'):
+        holdings = self.holdings
+        cash = self.cash
+        price_data = self.price_data.reset_index(drop=True)
+        strat = self.strat
+        objective = strat.objective
+
+        args = self._get_args()
+
+        signal_format = {
+            stock: 0 for stock in price_data.columns
+        }
+        signals = {}
+        for i in price_data.index:
+            signal = signal_format.copy()
+            for stock in signal_format.keys():
+                signal[stock] = objective(*[arg[stock] for arg in args], index=i)
+            signals[i] = signal
+
+        for i in signals.keys():
+            for stock in signals[i].keys():
+                self._trade(stock, price_data.loc[i, stock], signals[i][stock])
+
+        return self.cash, self.holdings
--- BtEngine/BtStrategy.py
+++ BtEngine/BtStrategy.py
@@ -2,23 +2,38 @@ import numpy as np
 import pandas as pd
 
 from typing import Literal
-
 class BtStrategy:
-    def __init__(self, strat: Literal['crossover', ...]):
+    def __init__(self,
+                 strat: Literal['crossover']  # More literals will be added as the strategies are implemented
+                 ) -> None:
+        self.strat = strat
         self._arg_signature = {
-            'crossover': ['sma', 'lma']  # prev averages can be accessed with .iloc
+            'crossover': ['sma', 'lma']  # prev averages can be accessed with pd.Series.iloc
         }
-
+        self.func_map = {
+            'crossover': self.crossover
+        }
+        self.objective = self.func_map[self.strat]
 
     @staticmethod
-    def crossover(sma, lma, prev_sma, prev_lma):
-        if prev_sma <= prev_lma and sma > lma:
+    def crossover(sma: pd.Series,
+                  lma: pd.Series,
+                  *,
+                  index: int  # Enumerate date indices to support iloc. BtEngine._iterate won't traverse DateIndex
+                  ) -> int:
+
+        curr_sma = sma.iloc[index]
+        curr_lma = lma.iloc[index]
+
+        prev_sma = sma.iloc[index-1]
+        prev_lma = lma.iloc[index-1]
+
+        if curr_sma > curr_lma and prev_sma <= prev_lma:
             return 1
-        elif prev_sma >= prev_lma and sma < lma:
+        elif curr_sma < curr_lma and prev_sma >= prev_lma:
             return -1
         else:
             return 0
-
 
     @property
     def arg_signature(self) -> list:
